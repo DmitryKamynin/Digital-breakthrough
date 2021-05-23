ede9b35e2d5443514b0317f0f3be08b068b131ee
DmitryKaminin <dak97dak@gmail.com>
2021-05-23 08:57:20 +0300

    fix route

--- front-aero/src/routes/index.js
+++ front-aero/src/routes/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Switch } from 'react-router';
-import {BrowserRouter as Router } from 'react-router-dom';
+// import {BrowserRouter as Router } from 'react-router-dom';
 
 import routes from '../constants/routes'; 
 
@@ -14,14 +14,14 @@ import Persons from '../screens/Persons'
 const AppRoutes = () => {
 
     return (
-        <Router>
+  
             <Switch>
                 <PublicRouter exact path={routes.getTasks()} component={Tasks} />
                 <PublicRouter exact path={routes.getMap()} component={ MapY } />
                 <PublicRouter exact path={routes.getUnits()} component={Persons} /> 
                 <PublicRouter exact path={routes.getTechnique()} component={Units} />
             </Switch>
-        </Router>
+ 
     )
 }  
 
